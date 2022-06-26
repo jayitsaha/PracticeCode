4f6cf8793d0f8152d9270e5d23dc55bbf7a9986a
Jayit Saha <45602643+ricksaha2000@users.noreply.github.com>
2022-06-27 03:42:13 +0530

    Time: 2 ms (90.12%), Space: 42.4 MB (74.32%) - LeetHub

---
+++ 67-add-binary/67-add-binary.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder bin=new StringBuilder();
+        
+        int i = a.length() - 1; //pointer 1
+        int j = b.length() - 1; //pointer 2
+        
+        int carry = 0; //carry is set to 0
+        
+//         now what we need to do is, start a while loop from the end of the two strings to the start
+//         and keep checking is any of the string still alive, if alive then we can add bub
+        
+        while(i>=0 || j>=0){
+            
+            int sum = carry; //initially set with carry, later we add the two more digits
+                
+            if(i>=0){ //basically if first string still alive
+                
+                sum+= a.charAt(i) - '0'; //so we need to do this - '0' because we converting to character
+                i = i-1;
+            }
+            
+            if(j>=0){ //basically if second string still alive
+                
+                sum+= b.charAt(j) - '0'; //so we need to do this - '0' because we converting to character
+                j = j-1;
+            }
+            
+//             now time to change carry based on the sum
+            
+            if(sum>1){
+                carry = 1;
+            }
+            else{
+                carry = 0;
+            }
+            
+//          now we know for 0 and 2, the result actually is 0, and for 1 and 3, the result is 1
+            
+            bin.append(sum%2);
+            
+            
+        }
+        
+        
+        
+        if(carry==1){
+            bin.append('1');
+        }
+        return bin.reverse().toString();
+    }
+}
