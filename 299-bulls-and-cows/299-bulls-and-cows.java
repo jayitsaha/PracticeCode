61a609b07d8a57f8f85302a01678d3248b833f43
Jayit Saha <45602643+ricksaha2000@users.noreply.github.com>
2022-06-27 04:37:23 +0530

    Time: 12 ms (40.61%), Space: 43.5 MB (17.30%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.java
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -6,31 +6,31 @@ class Solution {
         int len = secret.length();
         
         for(int i=0; i<len; i++) {
-            int digit = secret.charAt(i)-'0';
-            secretmap.put(digit, secretmap.getOrDefault(digit, 0)+1);
+            int key = secret.charAt(i)-'0';
+            secretmap.put(key, secretmap.getOrDefault(key, 0)+1);
         }
         
         int bull = 0; 
         int cow = 0; 
         
         for (int i=0; i<len; i++) {
-            int digit = guess.charAt(i)-'0';
-            int match =  secret.charAt(i)-'0';
+            int key = guess.charAt(i)-'0';
+            int sec =  secret.charAt(i)-'0';
             
-            if (digit == match) { 
-                secretmap.put(digit, secretmap.get(digit)-1);
+            if (key == sec) { 
+                secretmap.put(key, secretmap.get(key)-1);
                 bull++;
             } 
         }
         
         for (int i=0; i<len; i++) {
-            int digit = guess.charAt(i)-'0';
-            int match =  secret.charAt(i)-'0';
+            int key = guess.charAt(i)-'0';
+            int sec =  secret.charAt(i)-'0';
             
-            if (digit != match) {
-                if (secretmap.containsKey(digit) && secretmap.get(digit)>0) {
+            if (key != sec) {
+                if (secretmap.containsKey(key) && secretmap.get(key)>0) {
                     cow++;
-                    secretmap.put(digit, secretmap.get(digit)-1);
+                    secretmap.put(key, secretmap.get(key)-1);
                 }
             }
         }
